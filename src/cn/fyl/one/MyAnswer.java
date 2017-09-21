11566966bb1d9d82dec2d8ebd196cfdb977521cb
FYL <724319952@qq.com>
2017-09-21 09:46:08 +0800

    first answer

---
+++ src/cn/fyl/one/MyAnswer.java
@@ -0,0 +1,19 @@
+package cn.fyl.one;
+
+/**
+ * 在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+ * 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ * Created by Fang on 2017/9/21.
+ */
+public class MyAnswer {
+    public boolean Find(int target, int [][] array) {
+        for (int i = 0; i < array.length; i++){
+            for (int j = 0; j < array[i].length; j++){
+                if (array[i][j] == target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
